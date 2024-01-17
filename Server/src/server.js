0ac4c0960a21016321956d089dd550858613997d
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2024-01-16 23:15:40 -0500

    first steps in server

---
+++ Server/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
