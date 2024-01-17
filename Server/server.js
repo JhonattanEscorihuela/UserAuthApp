b6744922939cb8fa9e86b71e1571146cbb8ea224
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2024-01-16 23:44:29 -0500

    create of server and conection with DB

---
+++ Server/server.js
@@ -0,0 +1,12 @@
+const server = require("./src/app.js");
+require('dotenv').config();
+const { PORT } = process.env;
+const mongoose = require("./src/db.js");
+
+mongoose.connection.once('open', () => {
+    console.log('Mongo iniciado')
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listen in port http://localhost:${PORT}`)
+});
---
+++ Server/src/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: '../.env' });
+const { USER, PASSWORD, HOST } = process.env;
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    const con = await mongoose.connect(`mongodb+srv://jhonattan1410:${PASSWORD}@userauthapp.xxswn8u.mongodb.net/?retryWrites=true&w=majority`)
+    console.log(`mongodb connected: ${con.connection.host}`);
+    
+}
+
+module.exports = mongoose;
