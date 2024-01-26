72247ff9821a013a343394f00f3d6e9663e6df03
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2024-01-25 19:21:53 -0500

    change in front

--- Client/src/redux/services/api.js
+++ Client/src/redux/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://jhonattanescorihuela-userauthapp.onrender.com',
   headers: {
     'Authorization': `Bearer ${Cookies.get('authToken')}`,
   },
